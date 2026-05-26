0f57a6488e40f7f5526a0a7f5401fc8f2cd8c10a
cbensimon <charles@huggingface.co>
2026-05-26 15:43:34 +0000

    code from spaces + slight re-organize

---
+++ src/aokit/__init__.py
@@ -0,0 +1,240 @@
+"""
+"""
+
+import contextlib
+import inspect
+import os
+import textwrap
+from collections.abc import Iterable
+from contextvars import ContextVar
+from io import BytesIO
+from pathlib import Path
+from typing import Any
+from typing import Callable
+from typing import cast
+from typing import IO
+from unittest import mock
+
+import torch
+import torch._inductor.codecache # https://github.com/pytorch/pytorch/pull/165157
+from packaging import version
+from torch._functorch._aot_autograd.subclass_parametrization import unwrap_tensor_subclass_parameters
+from torch._inductor.cpu_vec_isa import valid_vec_isa_list
+from torch._inductor.package.package import package_aoti
+from torch.export.pt2_archive._package import AOTICompiledModel
+from torch.export.pt2_archive._package_weights import Weights
+
+from .elf import clear_elf_execstack
+from .zero import register_archive_cleanup
+
+
+INDUCTOR_CONFIGS_OVERRIDES: dict[str, Any] = {
+    'aot_inductor.package_constants_in_so': False,
+    'aot_inductor.package_constants_on_disk': True,
+    'aot_inductor.package': True,
+    'always_keep_tensor_constants': True,
+    'joint_graph_constant_folding': False,
+}
+
+if version.parse(version.parse(torch.__version__).base_version) >= version.parse('2.10'): # pragma: no cover
+    del INDUCTOR_CONFIGS_OVERRIDES['aot_inductor.package_constants_on_disk']
+    INDUCTOR_CONFIGS_OVERRIDES['aot_inductor.package_constants_on_disk_format'] = "pickle_weights"
+
+PACKAGE_DIRNAME = 'package'
+PACKAGE_FILENAME = 'package.pt2'
+
+
+def compile_and_save(
+    package_dir: str | os.PathLike[str],
+    exported_program: torch.export.ExportedProgram,
+    inductor_configs: dict[str, Any] | None = None,
+    submodule: str | None = None,
+):
+    archive_file = BytesIO()
+    files, _weights = _compile(exported_program, inductor_configs)
+    package_aoti(archive_file, list(files))
+    if submodule is not None:
+        subdir_path = Path(package_dir) / 'submodules' / submodule
+    else:
+        subdir_path = Path(package_dir) / 'root'
+    subdir_path.mkdir(parents=True, exist_ok=True)
+    package_path = subdir_path / PACKAGE_FILENAME
+    package_path.write_bytes(archive_file.getbuffer())
+
+
+def _compile(
+    exported_program: torch.export.ExportedProgram,
+    inductor_configs: dict[str, Any] | None = None,
+):
+    inductor_configs = {**(inductor_configs or {}), **INDUCTOR_CONFIGS_OVERRIDES}
+    gm = cast(torch.fx.GraphModule, exported_program.module())
+    assert exported_program.example_inputs is not None
+    args, kwargs = exported_program.example_inputs
+    artifacts = torch._inductor.aot_compile(gm, args, kwargs, options=inductor_configs) # pyright: ignore [reportArgumentType]
+    artifacts = cast(list[str | Weights], artifacts)
+    files = [file for file in artifacts if isinstance(file, str)]
+    for file in files:
+        if file.endswith('.so'):
+            clear_elf_execstack(Path(file))
+    weights, = (artifact for artifact in artifacts if isinstance(artifact, Weights))
+    return files, weights
+
+
+@contextlib.contextmanager
+def capture(
+    module: torch.nn.Module | Callable[..., Any],
+    call_method: str = 'forward',
+):
+    class CapturedCallException(Exception):
+        def __init__(self, *args, **kwargs):
+            super().__init__()
+            self.args = args
+            self.kwargs = kwargs
+
+    class CapturedCall:
+        def __init__(self):
+            self.args: tuple[Any, ...] = ()
+            self.kwargs: dict[str, Any] = {}
+
+    captured_call = CapturedCall()
+
+    def capture_call(*args, **kwargs):
+        raise CapturedCallException(*args, **kwargs)
+
+    with mock.patch.object(module, call_method, new=capture_call):
+        try:
+            yield captured_call
+        except CapturedCallException as e:
+            captured_call.args = e.args
+            captured_call.kwargs = e.kwargs
+
+
+class LazyAOTIModel:
+    def __init__(self, archive_file: torch.types.FileLike):
+        self.archive_file = archive_file
+        self.compiled_model: ContextVar[AOTICompiledModel | None] = ContextVar('compiled_model', default=None)
+        self.loaded_weights: ContextVar[dict[str, torch.Tensor] | None] = ContextVar('loaded_weights', default=None)
+        valid_vec_isa_list() # pre-warm functools.cache
+    def __call__(self, weights: dict[str, torch.Tensor], check_full_update: bool, *args, **kwargs):
+        if (compiled_model := self.compiled_model.get()) is None:
+            with register_archive_cleanup():
+                compiled_model = torch._inductor.aoti_load_package(self.archive_file)
+            compiled_model = cast(AOTICompiledModel, compiled_model)
+            self.compiled_model.set(compiled_model)
+        if (loaded_weights := self.loaded_weights.get()) is None or loaded_weights is not weights:
+            constant_fqns = compiled_model.get_constant_fqns()
+            constant_map = {name: tensor for name, tensor in weights.items() if name in constant_fqns}
+            # TODO: Explicit warn on missing / unexpected fqns
+            compiled_model.load_constants(constant_map, check_full_update=check_full_update, user_managed=True)
+            self.loaded_weights.set(weights)
+        return compiled_model(*args, **kwargs)
+    def with_weights(self, weights: dict[str, torch.Tensor]):
+        return LazyAOTIModelWithWeights(self, weights)
+
+
+class LazyAOTIModelWithWeights:
+    def __init__(self, model: LazyAOTIModel, weights: dict[str, torch.Tensor]):
+        self.model = model
+        self.weights = weights
+        self.first_call = True
+    def __call__(self, *args, **kwargs):
+        check_full_update = self.first_call
+        self.first_call = False
+        return self.model(self.weights, check_full_update, *args, **kwargs)
+
+
+def load(
+    module: torch.nn.Module,
+    repo_id: str,
+    revision: str | None = None,
+    aokit_loader: Callable[[torch.nn.Module, str], Any] | None = None,
+):
+    from huggingface_hub import snapshot_download
+    repo_path = snapshot_download(
+        repo_id=repo_id,
+        revision=revision,
+        allow_patterns=f'{PACKAGE_DIRNAME}/*',
+    )
+    package_dir = Path(repo_path) / PACKAGE_DIRNAME
+    if aokit_loader is not None:
+        aokit_loader(module, str(package_dir))
+    else:
+        load_from_package_dir(module, package_dir)
+
+
+def load_source_code(
+    module_expr: str,
+    repo_id: str,
+    revision: str | None = None,
+    aokit_loader: Callable[[torch.nn.Module, str], Any] | None = None,
+):
+    loader_name = aokit_loader.__name__ if aokit_loader is not None else 'None'
+    loader_source = ""
+    if aokit_loader is not None:
+        loader_source += textwrap.dedent(inspect.getsource(aokit_loader)).strip()
+        loader_source += "\n\n"
+    load_source = textwrap.dedent(
+        f"""\
+            aokit.load(
+                module={module_expr},
+                repo_id={repo_id!r},
+                revision={revision!r},
+                aokit_loader={loader_name},
+            )
+        """
+    )
+    load_source = '\n'.join(line for line in load_source.splitlines() if not "=None," in line)
+    return loader_source + load_source
+
+
+def load_from_package_dir(
+    module: torch.nn.Module | torch.nn.ModuleList,
+    package_dir: str | os.PathLike[str],
+):
+    # Structure
+    package_dir = Path(package_dir)
+    submodules_dir = package_dir / 'submodules'
+    rootmodule_dir = package_dir / 'root'
+    kernels_dir = package_dir / 'kernels'
+    # Submodules
+    if submodules_dir.is_dir():
+        for subpackage_dir in submodules_dir.iterdir():
+            if subpackage_dir.is_dir():
+                submodule = module.get_submodule(subpackage_dir.name)
+                load_from_module_dir(submodule, subpackage_dir)
+    # Root module
+    if rootmodule_dir.is_dir():
+        load_from_module_dir(module, rootmodule_dir)
+
+
+def load_from_module_dir(
+    module: torch.nn.Module | torch.nn.ModuleList,
+    module_dir: str | os.PathLike[str],
+):
+    module_dir = Path(module_dir)
+    aoti_model = LazyAOTIModel(module_dir / PACKAGE_FILENAME)
+    if isinstance(module, Iterable):
+        for block in module:
+            patch(block, aoti_model)
+    else:
+        patch(module, aoti_model)
+
+
+def patch(
+    module: torch.nn.Module,
+    aoti_model: LazyAOTIModel,
+    call_method: str = 'forward',
+):
+    module_ = _shallow_clone_module(module) # Prevent original module mutation
+    unwrap_tensor_subclass_parameters(module_) # https://github.com/pytorch/pytorch/issues/159918
+    aoti_model_with_weights = aoti_model.with_weights(module_.state_dict())
+    setattr(module, call_method, aoti_model_with_weights)
+
+
+def _shallow_clone_module(module: torch.nn.Module) -> torch.nn.Module:
+    clone = object.__new__(module.__class__)
+    clone.__dict__ = module.__dict__.copy()
+    clone._parameters = module._parameters.copy()
+    clone._buffers = module._buffers.copy()
+    clone._modules = {k: _shallow_clone_module(v) for k, v in module._modules.items() if v is not None}
+    return clone
